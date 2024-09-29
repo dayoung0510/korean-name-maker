4fdbc7638ff699c441e80800da526ea2ab51cdf8
dayoung0510 <day91@naver.com>
2024-09-29 19:40:42 +0900

    메타태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,15 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
         />
+        <meta property="og:title" content="우리말로 이름을 불러줘" />
+        <meta
+          property="og:description"
+          content="한글날 기념 우리말 이름 짓기"
+        />
+        <meta property="og:image" content="/images/ADC_img.png" />
+        <meta property="og:url" content="https://afterdinnerclub.kr/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="ko_KR" />
       </head>
       <body>
         <QueryClientProvider client={queryClient}>
